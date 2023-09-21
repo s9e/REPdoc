aee7b8ced95574eea5ae5de94c1dc3c2ed481962
JoshyPHP <s9e.dev@gmail.com>
2023-09-22 00:47:48 +0200

    WiP

--- src/Patch.php
+++ src/Patch.php
@@ -5,7 +5,7 @@
 * @copyright Copyright (c) The s9e authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
-namespace s9e\RegexpBuilder\Command;
+namespace s9e\REPdoc;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'repdoc:patch', description: 'Patches target files and directories.')]
+#[AsCommand(name: 'repdoc:patch', description: 'Patches target files and directories')]
 class Patch extends Command
 {
 	protected function configure(): void
@@ -32,7 +32,7 @@ class Patch extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$output->write(var_export($input->getOption('with-delimiters'), true));
+		$output->write(var_export($input->getOption('recursive'), true));
 
 		return Command::SUCCESS;
 	}
