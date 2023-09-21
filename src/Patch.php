da876069f31fb73a37832d26d1706628881c4459
JoshyPHP <s9e.dev@gmail.com>
2023-09-21 23:59:01 +0200

    WiP

---
+++ src/Patch.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/**
+* @package   s9e\REPdoc
+* @copyright Copyright (c) The s9e authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\RegexpBuilder\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'repdoc:patch', description: 'Patches target files and directories.')]
+class Patch extends Command
+{
+	protected function configure(): void
+	{
+		$this->addOption(
+			'recursive',
+			null,
+			InputOption::VALUE_NEGATABLE,
+			'Whether to recurse into directories',
+			true
+		);
+
+		$this->addArgument('targets', InputArgument::IS_ARRAY);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$output->write(var_export($input->getOption('with-delimiters'), true));
+
+		return Command::SUCCESS;
+	}
+}
