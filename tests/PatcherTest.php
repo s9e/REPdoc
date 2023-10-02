0ef9999c0b165c9ab947759f7482d6fa067a48c9
JoshyPHP <s9e.dev@gmail.com>
2023-10-02 22:17:59 +0200

    WiP

--- tests/PatcherTest.php
+++ tests/PatcherTest.php
@@ -10,28 +10,36 @@ use org\bovigo\vfs\vfsStreamDirectory;
 use s9e\REPdoc\EvalImplementation\NativeEval;
 use s9e\REPdoc\Filesystem;
 use s9e\REPdoc\MarkupProcessorRepository;
+use s9e\REPdoc\MarkupProcessor\Markdown;
 use s9e\REPdoc\Patcher;
 
 #[CoversClass('s9e\REPdoc\Patcher')]
 class PatcherTest extends TestCase
 {
 	#[DataProvider('getPatchFileTests')]
-	public function testPatchFile(array $originalFiles, array $expectedFiles)
+	public function testPatchFile(array $files)
 	{
 		vfsStream::setup('root');
-		foreach ($originalFiles as $filename => $contents)
-		{
-			file_put_contents(vfsStream::url('root/' . $filename), $contents);
-		}
+
+		$repository = new MarkupProcessorRepository;
+		$repository->addProcessor(new Markdown);
 
 		$patcher = new Patcher(
 			evalImplementation:  new NativeEval,
 			filesystem:          new Filesystem,
-			processorRepository: new MarkupProcessorRepository
+			processorRepository: $repository
 		);
-		foreach ($expectedFiles as $filename => $contents)
+		foreach ($files as $filename => [$original, $expected])
 		{
-			$this->assertStringEqualsFile(vfsStream::url('root/' . $filename), $contents);
+			$original = str_replace("\n\t\t\t\t\t\t", "\n", $original);
+			$expected = str_replace("\n\t\t\t\t\t\t", "\n", $expected);
+
+			$path    = vfsStream::url('root/' . $filename);
+			$changed = ($original !== $expected);
+			file_put_contents($path, $original);
+
+			$this->assertSame($changed, $patcher->patchFile($path));
+			$this->assertStringEqualsFile($path, $expected);
 		}
 	}
 
@@ -39,8 +47,29 @@ class PatcherTest extends TestCase
 	{
 		return [
 			[
-				['foo.exe' => ''],
-				['foo.exe' => '']
+				['foo.exe' => ['', '']]
+			],
+			[
+				[
+					'foo.md' => [
+						'..
+
+						```php
+						echo "Hello world.";
+						```
+						```plain
+						...
+						```',
+						'..
+
+						```php
+						echo "Hello world.";
+						```
+						```plain
+						Hello world.
+						```'
+					]
+				]
 			],
 		];
 	}
