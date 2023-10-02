fcc189cbf2c05e1b9958ce8215471b8ac4eae411
JoshyPHP <s9e.dev@gmail.com>
2023-10-02 17:02:34 +0200

    WiP

--- src/Command/Patch.php
+++ src/Command/Patch.php
@@ -17,6 +17,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\PhpProcess;
 use function array_merge, class_exists, count, get_class, implode;
+use s9e\REPdoc\EvalImplementation\EvalInterface;
 use s9e\REPdoc\EvalImplementation\NativeEval;
 use s9e\REPdoc\EvalImplementation\SymfonyProcess;
 use s9e\REPdoc\Filesystem;
@@ -28,6 +29,11 @@ use s9e\REPdoc\Patcher;
 #[AsCommand(name: 'repdoc:patch', description: 'Patches target files and directories')]
 class Patch extends Command
 {
+	protected EvalInterface $eval;
+	protected Filesystem $filesystem;
+	protected MarkupProcessorRepository $repository;
+	protected Patcher $patcher;
+	protected SymfonyStyle $io;
 	protected bool $hasSymfonyProcess;
 
 	protected function configure(): void
@@ -41,7 +47,6 @@ class Patch extends Command
 			'Whether to execute the PHP code in its own process (requires symfony/process)',
 			false
 		);
-
 		$this->addOption(
 			'recursive',
 			null,
@@ -49,74 +54,46 @@ class Patch extends Command
 			'Whether to recurse into directories',
 			true
 		);
-
-		$this->addArgument('targets', InputArgument::IS_ARRAY);
+		$this->addArgument('targets', InputArgument::IS_ARRAY | InputArgument::REQUIRED);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$io               = new SymfonyStyle($input, $output);
+		$this->io         = new SymfonyStyle($input, $output);
 		$recursive        = (bool) $input->getOption('recursive');
 		$targets          = (array) $input->getArgument('targets');
 		$processIsolation = (bool) $input->getOption('process-isolation');
 
-		if (empty($targets))
-		{
-			$io->error('No targets provided');
-
-			return Command::FAILURE;
-		}
-		if ($processIsolation && !$this->hasSymfonyProcess)
-		{
-			$io->error('Cannot use process isolation, make sure symfony/process is installed');
-
-			return Command::FAILURE;
-		}
-
-		$eval       = $processIsolation ? new SymfonyProcess : new NativeEval;
-		$filesystem = new Filesystem;
-		$repository = new MarkupProcessorRepository;
-		$patcher    = new Patcher(
-			evalImplementation:  $eval,
-			filesystem:          $filesystem,
-			processorRepository: $repository
+		$this->eval       = $this->getEvalImplementation($processIsolation);
+		$this->filesystem = new Filesystem;
+		$this->repository = $this->getMarkupProcessorRepository();
+		$this->patcher    = new Patcher(
+			evalImplementation:  $this->eval,
+			filesystem:          $this->filesystem,
+			processorRepository: $this->repository
 		);
 
-		$repository->addProcessor(new Html);
-		$repository->addProcessor(new Markdown);
+		// Gather/display the list of supported files
+		$extensions = $this->repository->getSupportedFileExtensions();
+		$this->printSupportedFileExtensions($extensions);
 
-		$extensions = $repository->getSupportedFileExtensions();
-		$io->writeln('Supported file extensions: ' . implode(', ', $extensions));
-		if ($io->isVerbose())
-		{
-			$rows = [];
-			foreach ($extensions as $ext)
-			{
-				$rows[] = [$ext, get_class($repository->getProcessorForFileExtension($ext))];
-			}
-
-			$io->table(
-				['File extension', 'Markup processor class'],
-				$rows
-			);
-		}
-
+		// Collect the list of supported files among targets
 		$paths = [];
 		foreach ($targets as $target)
 		{
-			$io->writeln('Looking for supported files in ' . $target);
-			$targetFilepaths = $filesystem->getFilepaths($target, $extensions, $recursive);
-			$io->writeln('Files found: ' . count($targetFilepaths));
+			$this->io->writeln('Looking for supported files in ' . $target, OutputInterface::VERBOSITY_VERBOSE);
+			$targetFilepaths = $this->filesystem->getFilepaths($target, $extensions, $recursive);
+			$this->io->writeln('Files found: ' . count($targetFilepaths), OutputInterface::VERBOSITY_VERBOSE);
 			$paths = array_merge($paths, $targetFilepaths);
 
 			foreach ($targetFilepaths as $path)
 			{
-				$io->writeln('Found ' . $path, OutputInterface::VERBOSITY_VERBOSE);
+				$this->io->writeln('Found ' . $path, OutputInterface::VERBOSITY_VERY_VERBOSE);
 			}
 		}
 
 		$infoSection = $output->section();
-		if (!$io->isVerbose())
+		if (!$this->io->isVeryVerbose())
 		{
 			$infoSection->setMaxHeight(1);
 		}
@@ -129,7 +106,7 @@ class Patch extends Command
 		foreach ($paths as $path)
 		{
 			$infoSection->writeln('Patching ' . $path);
-			if ($patcher->patchFile($path))
+			if ($this->patcher->patchFile($path))
 			{
 				$changed[] = $path;
 				$infoSection->writeln('Updated ' . $path, OutputInterface::VERBOSITY_VERBOSE);
@@ -141,8 +118,47 @@ class Patch extends Command
 			$progressBar->advance();
 		}
 
-		$io->success('Files changed: ' . count($changed) . ' / ' . count($paths));
+		$this->io->success('Files changed: ' . count($changed) . ' / ' . count($paths));
 
 		return Command::SUCCESS;
+	}
+
+	protected function getMarkupProcessorRepository(): MarkupProcessorRepository
+	{
+		$repository = new MarkupProcessorRepository;
+		$repository->addProcessor(new Html);
+		$repository->addProcessor(new Markdown);
+
+		return $repository;
+	}
+
+	protected function getEvalImplementation(bool $processIsolation): EvalInterface
+	{
+		if ($processIsolation && !$this->hasSymfonyProcess)
+		{
+			$this->io->error('Cannot use process isolation, make sure symfony/process is installed');
+
+			return Command::FAILURE;
+		}
+
+		return $processIsolation ? new SymfonyProcess : new NativeEval;
+	}
+
+	protected function printSupportedFileExtensions(array $extensions): void
+	{
+		$this->io->writeln('Supported file extensions: ' . implode(', ', $extensions), OutputInterface::VERBOSITY_VERBOSE);
+		if ($this->io->isVeryVerbose())
+		{
+			$rows = [];
+			foreach ($extensions as $ext)
+			{
+				$rows[] = [$ext, get_class($this->repository->getProcessorForFileExtension($ext))];
+			}
+
+			$this->io->table(
+				['File extension', 'Markup processor class'],
+				$rows
+			);
+		}
 	}
 }
