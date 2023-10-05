4498771fb2411add269a978290bb43b4e660d5e4
JoshyPHP <s9e.dev@gmail.com>
2023-10-05 17:16:27 +0200

    WiP

---
+++ tests/Exception/EvalExceptionTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace s9e\REPdoc\Tests\Exception;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use s9e\REPdoc\Exception\EvalException;
+
+#[CoversClass('s9e\REPdoc\Exception\EvalException')]
+class EvalExceptionTest extends TestCase
+{
+	public function testSetSourceCode(): void
+	{
+		$e = new EvalException;
+		$e->setSourceCode('echo $x;');
+
+		$this->assertEquals('echo $x;', $e->sourceCode);
+	}
+
+	public function testGetSourceCode(): void
+	{
+		$e = new EvalException;
+		$e->setSourceCode('echo $x;');
+
+		$this->assertEquals('echo $x;', $e->getSourceCode());
+	}
+}
